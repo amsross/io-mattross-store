9a1630d4d642f47fd47d0e8f7325ffd8d17723ba
Matt Ross <matt@mattross.io>
2014-02-25 14:24:46 -0500

    Alphabetize Grunt NpmTasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -166,10 +166,10 @@ module.exports = function(grunt) {
 
 	//Load NPM tasks
 	grunt.loadNpmTasks('grunt-concurrent');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-env');
