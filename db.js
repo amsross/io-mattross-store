39ec1b4a5721d20231ac2f63f1adab97a84b291c
Matt Ross <matt@mattross.io>
2014-04-23 13:24:11 +0000

    Add mongodb for travis tests

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGOHQ_URL);
+mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://test_user:test_pass@localhost:27017/test_db');
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 
 module.exports = mongoose.connection;
